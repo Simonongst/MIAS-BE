193abb116d47f1ed0a5399ca69e69567e9682149
Simon Ong <simonongst@gmail.com>
2025-10-27 00:06:17 +0800

    feat: initial Express and MongoDB setup with environment config

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+
+const connectDB = require("./db/db.js")
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+
+const PORT = process.env.PORT ? process.env.PORT : "3000";
+
+app.listen(PORT, () => {
+    console.log(`The express app is ready on port ${PORT}!`);
+});
