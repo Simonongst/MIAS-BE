eaa94733527edf6beaa7c070efece3868c8970d9
Simon Ong <simonongst@gmail.com>
2025-10-31 23:07:51 +0800

    feat(auth): add user sign-in with JWT access and refresh tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,4 +25,37 @@ const signUp = async (req, res) => {
     }
 };
 
-module.exports = { signUp };
+const signIn = async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.eid });
+        if(!userInDatabase) {
+            return res.status(401).send("Sign-in failed. Invalid Enterprise ID or Password.");
+        };
+
+        const validPassword = bcrypt.compareSync(
+            req.body.password,
+            userInDatabase.password
+        );
+        if(!validPassword) {
+            return res.status(401).send("Login failed. Invalid Enterprise ID or Password.");
+        };
+
+        const claims = { username: userInDatabase.username };
+
+        const access = jwt.sign(claims, process.env.ACCESS_SECRET, {
+            expiresIn: "15m",
+            jwtid: uuidv4(),
+        });
+
+        const refresh = jwt.sign(claims, process.env.REFRESH_SECRET, {
+            expiresIn: "30d",
+            jwtid: uuidv4(),
+        });
+
+        res.status(200).json({ access, refresh });
+    } catch (err) {
+        res.status(500).send({ err: err.message }).redirect('/');
+    }
+};
+
+module.exports = { signUp, signIn };
--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { signUp } = require("../controllers/authController.js");
+const { signUp, signIn } = require("../controllers/authController.js");
 
 router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
 
 module.exports = router;
