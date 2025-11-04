4b8e32aac14d4c9c07439947642aa812d3863160
Simon Ong <simonongst@gmail.com>
2025-11-04 21:07:12 +0800

    git commit -m "chore: assign mongoose.model to Invoice and export it"

--- models/invoice.js
+++ models/invoice.js
@@ -29,4 +29,6 @@ const invoiceSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model("Invoice", invoiceSchema);
+const Invoice = mongoose.model("Invoice", invoiceSchema);
+
+module.exports = Invoice;
