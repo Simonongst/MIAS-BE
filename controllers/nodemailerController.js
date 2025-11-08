b59aedae19360b3b234f8462197216913e882f8b
sheena <syeongsn@gmail.com>
2025-11-09 01:25:47 +0800

    feat: set up nodemailer with a working gmail account and successfully sent email

--- controllers/nodemailerController.js
+++ controllers/nodemailerController.js
@@ -1,45 +1,18 @@
 const nodemailer = require('nodemailer');
 
-let transporter;
-
-(async () => {
-  try {
-    const testAccount = await nodemailer.createTestAccount();
-
-    transporter = nodemailer.createTransport({
-      host: testAccount.smtp.host,
-      port: testAccount.smtp.port,
-      secure: testAccount.smtp.secure,
-      auth: {
-        user: testAccount.user,
-        pass: testAccount.pass,
-      },
-    });
-
-    console.log('Ethereal test account ready:', testAccount.user);
-  } catch (err) {
-    console.error('Failed to create Ethereal account:', err);
-  }
-})();
-
-// Real account
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.ethereal.email",
-//   port: 587,
-//   secure: false, // true for 465, false for other ports
-//   auth: {
-//     user: "maddison53@ethereal.email",
-//     pass: "jn7jnAPss4f63QBp6D",
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 const sendEmail = async (req, res) => {
   try {
-    if (!transporter) throw new Error('Transporter not initialized yet');
-
     const info = await transporter.sendMail({
-      from: '"Maddison Foo Koch" <test@ethereal.email>',
-      to: 'bar@example.com, baz@example.com',
+      from: process.env.EMAIL_USERNAME,
+      to: 'syeongsn@gmail.com',
       subject: 'Hello ✔',
       text: 'Hello world?',
       html: '<b>Hello world?</b>',
